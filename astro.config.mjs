62e8a736dcc513da4abaddba420ba03167be6f1c
Xabi <xezpeleta@gmail.com>
2023-01-30 21:01:51 +0100

    site url changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
     // Enable React for the Algolia search component.
     react(),
   ],
-  site: 'https://tknika.github.io',
-  base: 'normadera',
+  site: 'https://normadera.fpeuskadi.eus',
+  base: '',
 });
